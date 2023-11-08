d026ad53a3a19d8b4706a4f30d82da0c5a214b18
Ionuț Hoară <87201884+HoaraIonut@users.noreply.github.com>
2023-11-08 17:20:00 +0200

    Update rezolvareLab5.py

--- Laborator 5/rezolvareLab5.py
+++ Laborator 5/rezolvareLab5.py
@@ -1,8 +1,6 @@
-#mai am de rezolvat exercitiul 2
-
+#EX1 a)
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 def MetNaiva(f, a, b, n, x):
     X = np.linspace(a, b, n + 1)
@@ -12,27 +10,16 @@ def MetNaiva(f, a, b, n, x):
     C = np.flip(C)
     y = 0
     for i in range(n + 1):
-        y = y + C[i] * x ** i
+        y += C[i] * x **i
     return y
 
+#EX1 b)
+
+def f(x):
+    return np.exp(2 * x)
 
 a = -1
 b = 1
-n = 4
-
-
-def f(x):
-    return np.e ** (2 * x)
-
-
-x_vector = np.linspace(a, b)
-
-y = [MetNaiva(f, a, b, n, x) for x in x_vector]
-
-plt.plot(x_vector, y, c='b', linewidth=2)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
 
 
 def PlotInterpolare(f, a, b, n, metoda):
@@ -40,30 +27,34 @@ def PlotInterpolare(f, a, b, n, metoda):
     x_grafic = np.linspace(a, b, 50 * n + 1)
     Pn = []
     for x in x_grafic:
-        Pn.append(metoda(f, a, b, n, x))
-    plt.plot(x_grafic, Pn, c='r', linewidth=1, linestyle='dotted')
-    plt.plot(x_grafic, f(x_grafic), c='black', linewidth=1)
-    plt.scatter(X, f(X))
-    return x
+        Pn.append(MetNaiva(f, a, b, n, x))
+    plt.plot(x_grafic, Pn, c='r', linestyle='dotted', linewidth=1)
+    plt.plot(x_grafic, f(x_grafic), c='b', linewidth=1)
+    plt.legend(['Functia aproximata', 'Functia f'])
+    for el in X:
+        plt.scatter(el, f(el), c='blue')
+    plt.legend(['Functia aproximata', 'Functia f'])
+    plt.show()
 
+# for i in range(1, 5):
+#     plt.title("Polinomul de interpolare Lagrange Pn(x) unde n = {sval}".format(sval=i))
+#     PlotInterpolare(f, a, b, i, MetNaiva)
+
+
+#EX1 c)
 def ErrAbs(f, a, b, n, metoda):
-    X = np.linspace(a, b, n + 1)
-    x_grafic = np.linspace(a, b, 50 * n + 1)
-    Pn = []
+    x_grafic = np.linspace(a, b, 50 * (n + 1))
     err = []
     for x in x_grafic:
-        err.append(f(x) - metoda(f, a, b, n, x))
-    plt.plot(x_grafic, err, c='r', linewidth=1, linestyle='dotted')
-    #plt.plot(x_grafic, f(x_grafic), c='black', linewidth=1)
-    plt.scatter(X, f(X))
-    return x
-
-for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetNaiva)
-    plt.title('n = ' + str(i))
+        el = abs(f(x) - MetNaiva(f, a, b, n, x))
+        # print("ErrAbs = ", f(x) - el)
+        err.append(el)
+    plt.plot(x_grafic, err)
     plt.show()
 
 for i in range(1, 5):
-    ErrAbs(f, a, b, i, MetNaiva)
-    plt.title('n = ' + str(i))
-    plt.show()
+    plt.title("ErrAbs(x)=|f(x)-Pn(x)| pt n = {val}".format(val=i))
+    ErrAbs(f, -1, 1, i, MetNaiva)
+
+#EX2 a)
+
