aee3231cee388a91075c56f2ca240ce88307ad18
Ionuț Hoară <87201884+HoaraIonut@users.noreply.github.com>
2024-01-01 22:13:41 +0200

    Update rezolvareLab5.py

--- Laborator 5/rezolvareLab5.py
+++ Laborator 5/rezolvareLab5.py
@@ -1,70 +1,86 @@
+#Rezolvare Lab5
+
 import numpy as np
-import matplotlib.pyplot as plt
+import  matplotlib.pyplot as plt
+
+#Rezolvare exc 1
+
+def f(x):
+    return np.exp(2 * x)
+
+a = -1
+b = 1
 
 def MetNaiva(f, a, b, n, x):
     X = np.linspace(a, b, n+1)
     Y = f(X)
+
     A = np.vander(X)
-    C = np.linalg.solve(A, Y)
-    C = np.flip(C)
+    C = np.flip(np.linalg.solve(A, Y))
+
     y = 0
-    for i in range(n+1):
+    for i in range(n + 1):
         y += C[i] * x ** i
     return y
 
-def f(x):
-    return np.exp(2*x)
-#
-a = - 1
-b = 1
-
-def PlotInterpolare(f, a, b, n, metoda):
-    plt.title('n = {val}'.format(val=n))
-    X = np.linspace(a, b, n+1)
-    x_grafic = np.linspace(a, b, 50*n+1)
+def PlotInterpolareMetNaiva(f, a, b, n, metoda):
+    X = np.linspace(a, b, n + 1)
+    X_grafic = np.linspace(a, b, 50 * n + 1)
     Pn = []
-    for x in x_grafic:
+
+    for x in X_grafic:
         Pn.append(metoda(f, a, b, n, x))
-    plt.plot(x_grafic, f(x_grafic), c='g', linewidth=1)
-    plt.plot(x_grafic, Pn, c='r', linestyle='dotted', linewidth=1)
-    for el in X:
-        plt.scatter(el, f(el), c='b')
-    plt.legend(['Functia f', 'Polinomul de interpolare Lagrange'])
+
+    plt.subplot(1, 2, 1)
+    plt.title(" (Met Naiva) n = {}".format(n))
+    plt.plot(X_grafic, f(X_grafic), c='r', linewidth=1)
+    plt.plot(X_grafic, Pn, c='b', linewidth=1)
+
+    plt.scatter(X, f(X), c='y')
+
+    plt.subplot(1, 2, 2)
+    plt.title("(Met Naiva) Eroarea absoluta pt n = {}".format(n))
+    plt.plot(X_grafic, abs(f(X_grafic) - Pn))
+
     plt.show()
 
-for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetNaiva)
-
-def PlotInterpolareEroare(f, a, b, n, metoda):
-    plt.title('Eroare absoluta pt n = {val}'.format(val=n))
-    X = np.linspace(a, b, n+1)
-    x_grafic = np.linspace(a, b, 50*n+1)
-    Pn = []
-    for x in x_grafic:
-        Pn.append(metoda(f, a, b, n, x))
-    plt.plot(x_grafic, abs(f(x_grafic) - Pn), c='r')
-    plt.show()
-
-for i in range(1, 5):
-    PlotInterpolareEroare(f, a, b, i, MetNaiva)
-
-#Rezolvare Ex2
+#Rezolvare exc 2
 
 def MetLagrange(f, a, b, n, x):
     X = np.linspace(a, b, n + 1)
-    Y = f(X)
-    y = 0
-    for k in range(n+1):
-        L = 1
-        for i in range(n + 1):
+    def L(n, k):
+        P1 = 1
+        P2 = 1
+        for i in range(n+1):
             if i != k:
-                L = L * (x-X[i])/(X[k] - X[i])
-        y = y + L * Y[k]
+                P1 *= (x - X[i])
+                P2 *= (X[k] - X[i])
+        return P1/P2
+
+    y = 0
+    for k in range(0, n+1):
+        y += (L(n, k) * f(X[k]))
+
     return y
 
-for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetLagrange)
+def PlotInterpolareLagrange(f, a, b, n, metoda):
+    X = np.linspace(a, b, n+1)
+    X_grafic = np.linspace(a, b, 50*n+1)
+    Pn = []
+
+    for x in X_grafic:
+        Pn.append(metoda(f, a, b, n, x))
+
+    plt.subplot(1, 2, 1)
+    plt.title("(Metoda Lagrange) n = {}".format(n))
+    plt.plot(X_grafic, Pn, c='r')
+    plt.plot(X_grafic, f(X_grafic), c='b')
+    plt.scatter(X, f(X), c='y')
+
+    plt.subplot(1, 2, 2)
+    plt.title("(Metoda Lagrange) Eroarea absoluta pt n = {}".format(n))
+    plt.plot(X_grafic, abs(f(X_grafic)-Pn))
+    plt.show()
 
 for i in range(1, 5):
-    PlotInterpolareEroare(f, a, b, i, MetLagrange)
-
+    PlotInterpolareLagrange(f, a, b, i, MetLagrange)
