da094808e376460d4c6b2f9568c78241b2c589c6
Ionuț Hoară <87201884+HoaraIonut@users.noreply.github.com>
2024-01-02 13:34:51 +0200

    Update rezolvareLaborator6.py

--- Laborator 6/rezolvareLaborator6.py
+++ Laborator 6/rezolvareLaborator6.py
@@ -1,97 +1,132 @@
-#Rezolvare Exc1
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def MetNeville(f, a, b, n, x):
-    X = np.linspace(a, b, n + 1)
-    Q = np.zeros((n + 1, n + 1))
-    for i in range(n + 1):
-        Q[i][0] = f(X[i])
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
-            Q[i][j] = (Q[i][j-1] * (x - X[i - j]) - Q[i - 1][j - 1] * (x - X[i])) / (X[i] - X[i-j])
-    y = Q[n][n]
-    return y
-
 def f(x):
-    return np.exp(2*x)
-#
-a = - 1
+    return np.exp(2 * x)
+
+a = -1
 b = 1
 
-def PlotInterpolare(f, a, b, n, metoda):
-    plt.title('n = {}'.format(n))
-
+def MetNeville(f, a, b, n, x):
     X = np.linspace(a, b, n+1)
-    x_grafic = np.linspace(a, b, 50*n+1)
+    Q = np.zeros((n+1, n+1))
+    for i in range(n+1):
+        Q[i, 0] = f(X[i])
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            Q[i, j] = (Q[i, j-1] * (x-X[i-j]) - Q[i-1, j-1]*(x-X[i])) / (X[i] - X[i-j])
+    return Q[n, n]
+
+def PlotInterpolareMetNeville(f, a, b, n, metoda):
+    X = np.linspace(a, b, n+1)
+    X_grafic = np.linspace(a, b, 50*n+1)
     Pn = []
-    for x in x_grafic:
+
+    for x in X_grafic:
         Pn.append(metoda(f, a, b, n, x))
-    plt.plot(x_grafic, f(x_grafic), c='g', linewidth=1)
-    plt.plot(x_grafic, Pn, c='r', linestyle='dotted', linewidth=1)
-    for el in X:
-        plt.scatter(el, f(el), c='b')
-    plt.legend(['Functia f', 'Polinomul de interpolare Lagrange'])
+
+    plt.subplot(1, 2, 1)
+    plt.title("(Met Neville) n = {}".format(n))
+    plt.plot(X_grafic, Pn, c='r')
+    plt.plot(X_grafic, f(X_grafic), c='b')
+    plt.scatter(X, f(X), c='y')
+
+    plt.subplot(1, 2, 2)
+    plt.title("(Met Neville) Eroare absoluta pt n = {}".format(n))
+    plt.plot(X_grafic, abs(f(X_grafic) - Pn))
     plt.show()
 
-for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetNeville)
+# for i in range(1, 5):
+#     PlotInterpolareMetNeville(f, a, b, i, MetNeville)
 
-def PlotInterpolareEroare(f, a, b, n, metoda):
-    plt.title('Eroare absoluta pt n = {val}'.format(val=n))
-    X = np.linspace(a, b, n+1)
-    x_grafic = np.linspace(a, b, 50*n+1)
-    Pn = []
-    for x in x_grafic:
-        Pn.append(metoda(f, a, b, n, x))
-    plt.plot(x_grafic, abs(f(x_grafic) - Pn), c='r')
-    plt.show()
-
-for i in range(1, 5):
-    PlotInterpolareEroare(f, a, b, i, MetNeville)
 
 #Rezolvare Exc2
 
 def MetNewton(f, a, b, n, x):
+    X = np.linspace(a, b, n + 1)
+    c = f(X[0])
+
+    def C(k):
+        p = 1
+
+        for i in range(k):
+            p *= (X[k] - X[i])
+
+        return (f(X[k]) - P(k - 1, X[k])) / p
+
+    def P(k, x):
+        if k == 0:
+            return c
+        else:
+            p = 1
+            for i in range(k):
+                p *= (x - X[i])
+            return P(k - 1, x) + C(k) * p
+
+    return P(n, x)
+
+def PlotInterpolareMetNewton(f, a, b, n, metoda):
     X = np.linspace(a, b, n+1)
-    Y = f(X)
-    def Pk(k, x):
-       if k == 0:
-           return Y[0]
-       else:
-           p1 = np.prod([X[k] - X[i] for i in range(k)])
-           p2 = np.prod([x - X[i] for i in range(k)])
-           ck = (Y[k] - Pk(k - 1, X[k])) / p1
-           return Pk(k-1, x) + ck * p2
-    return Pk(n, x)
+    X_grafic = np.linspace(a, b, 50 * (n+1))
+    Pn = []
 
-for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetNewton)
+    for x in X_grafic:
+        Pn.append(metoda(f, a, b, n, x))
 
-for i in range(1, 5):
-    PlotInterpolareEroare(f, a, b, i, MetNewton)
+    plt.subplot(1, 2, 1)
+    plt.title("(Met Newton) pt n = {}".format(n))
+    plt.plot(X_grafic, Pn, c='r')
+    plt.plot(X_grafic, f(X_grafic), c='b')
+    plt.scatter(X, f(X), c='y')
 
-#Rezolvare Exc3
+    plt.subplot(1, 2 ,2)
+    plt.title("(Met Newton) Eroarea absoluta pt n={}".format(n))
+    plt.plot(X_grafic, abs(f(X_grafic) - Pn))
+    plt.show()
+
+
+# for i in range(1, 5):
+#     PlotInterpolareMetNewton(f, a, b, i, MetNewton)
 
 def MetNewtonDD(f, a, b, n, x):
     X = np.linspace(a, b, n+1)
-    Y = f(X)
-    def DD(arr):
-        if len(arr) == 1:
-            return f(arr[0])
+    q = f(X[0])
+
+    def c(inc, fin):
+        if(inc == fin):
+            return f(X[inc])
         else:
-            return (DD(arr[1:]) - DD(arr[0:-1])) / (arr[-1] - arr[0])
-    def Pk(k, x):
+            return (c(inc+1, fin) - c(inc, fin-1))/(X[fin] - X[inc])
+
+    def P(k, x):
         if k == 0:
-            return Y[0]
+            return q
         else:
-            produs = np.prod([x - X[i] for i in range(k)])
-            return Pk(k-1, x) + DD(X[:k+1]) * produs
-    return Pk(n, x)
+            p = 1
+            for i in range(k):
+                p *= (x - X[i])
+            return P(k - 1, x) + (c(0, k) * p)
+
+    return P(n, x)
+
+def PlotInterpolareMetNewtonDD(f, a, b, n, metoda):
+    X = np.linspace(a, b, n+1)
+    X_grafic = np.linspace(a, b, 50*(n+1))
+    Pn = []
+
+    for x in X_grafic:
+        Pn.append(metoda(f, a, b, n, x))
+
+    plt.subplot(1, 2, 1)
+    plt.title("(Met Newton DD) n = {}".format(n))
+    plt.plot(X_grafic, f(X_grafic), c='r')
+    plt.plot(X_grafic, Pn, c='b')
+    plt.scatter(X, f(X), c='y')
+
+    plt.subplot(1, 2, 2)
+    plt.title("(Met Newton DD) Eroare abs pt n = {}".format(n))
+    plt.plot(X_grafic, abs(f(X_grafic) - Pn))
+    plt.show()
 
 for i in range(1, 5):
-    PlotInterpolare(f, a, b, i, MetNewtonDD)
-
-for i in range(1, 5):
-    PlotInterpolareEroare(f, a, b, i, MetNewtonDD)
+    PlotInterpolareMetNewtonDD(f, a, b, i, MetNewtonDD)
